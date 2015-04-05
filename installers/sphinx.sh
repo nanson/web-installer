4c2b2ae62136e8dfd0e5382b1f97d70043b2e45f
nanson <nanson@rzncenter.ru>
2015-04-05 19:01:33 +0300

    Обновление пакетов

--- installers/sphinx.sh
+++ installers/sphinx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Установка
-yum install -y http://sphinxsearch.com/files/sphinx-2.2.6-1.rhel7.x86_64.rpm
+yum install -y http://sphinxsearch.com/files/sphinx-2.2.8-1.rhel7.x86_64.rpm
 
 # Настройка
 /bin/cp ../configs/sphinx/sphinx.conf /etc/sphinx/sphinx.conf
